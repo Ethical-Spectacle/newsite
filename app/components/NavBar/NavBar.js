553e9bc0a56c8260ddb9a3501383b85d035d8c21
Cristian Zambrano <criszamcham@gmail.com>
2024-04-18 22:27:25 -0700

    Update nav bar

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import navlogo from '../../assets/navlogo.svg'
+import whitelogo from '../../assets/whitelogo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import Button from '../Button'
@@ -15,13 +15,13 @@ function NavBar() {
     setShowMobileMenu(!showMobileMenu);
   };
 
-  const hoverClasses = 'hover:font-semibold hover:underline hover:decoration-rose-400 hover:underline-offset-4'
+  const hoverClasses = 'text-slate-50 hover:font-semibold hover:underline hover:decoration-rose-400 hover:underline-offset-4'
 
   return (
-    <nav className='w-full fixed justify-center bg-slate-50'>
-      <div className='max-w-1440 p-2 flex md:flex-row justify-between border-b border-black mx-auto'>
+    <nav className='w-full fixed justify-center bg-slate-800 drop-shadow-md'>
+      <div className='max-w-1440 p-2 flex md:flex-row justify-between mx-auto'>
         <Link href="/">
-          <Image src={navlogo} alt="Ethical Spectacle Research" width={70} height={70} />
+          <Image src={whitelogo} alt="Ethical Spectacle Research" width={70} height={70} />
         </Link>
         <ul className='space-x-4 hidden md:flex text-black space-x-7'>
           <li className={hoverClasses}>
@@ -35,7 +35,7 @@ function NavBar() {
           </li>
         </ul>
         <Button title='Log in' additionalClass='hidden'/>
-        <div className='w-7 h-7 md:hidden' onClick={toggleMobileMenu}>
+        <div className='w-7 h-7 text-slate-50 md:hidden' onClick={toggleMobileMenu}>
           <FontAwesomeIcon icon={faBars} className='w-full h-full' />
         </div>
       </div>
