12dbb30588e67d720cb6a657ca85b012d7987e22
Cristian Zambrano <criszamcham@gmail.com>
2024-04-23 15:33:44 -0700

    Add box styling

--- app/components/Home/Contact.js
+++ app/components/Home/Contact.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className="w-full flex justify-center mb-10">
-        <section className="w-full max-w-1440 flex min-h-[50vh] flex-col justify-center items-center mx-5 p-2 bg-slate-800 text-cream space-y-10 pink-box-shadow-style">
-            <h1 className='text-cream'>Contact Us</h1> 
-            <p className='text-center text-lg md:text-2xl'>
-                Interested in joining our mission or have questions? Contact our president Maximus Powers at 
-                <a href='mailto:maximus@ethicalspectacle.com' className='underline pl-2'>
-                  maximus@ethicalspectacle.com
-                </a>
-            </p>
-        </section>  
+    <section className="w-full flex justify-center mb-10 relative">
+        <section className="shape-wrap w-full max-w-1440 flex min-h-[50vh] flex-col justify-center items-center mx-5 text-cream relative">
+            <div className='bg-slate-800 w-full h-full flex flex-col justify-center items-center space-y-10 z-10'>
+              <h1 className='text-cream'>Contact Us</h1> 
+              <p className='text-center text-lg md:text-2xl'>
+                  Interested in joining our mission or have questions? Contact our president Maximus Powers at 
+                  <a href='mailto:maximus@ethicalspectacle.com' className='underline pl-2'>
+                    maximus@ethicalspectacle.com
+                  </a>
+              </p>
+            </div>
+
+        </section>
     </section>
   )
 }
