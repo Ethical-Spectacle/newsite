dcb0761e195e8d000adea37944fc154f3fab5c1e
Cristian Zambrano <criszamcham@gmail.com>
2024-04-23 13:43:08 -0700

    Add extra styling to contact us

--- app/components/Home/Contact.js
+++ app/components/Home/Contact.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Contact() {
   return (
     <section className="w-full flex justify-center mb-10">
-        <section className="w-full max-w-1440 flex min-h-[50vh] flex-col justify-center items-center mx-5 p-2 bg-slate-800 text-cream space-y-10">
-            <h1 className='text-cream'>Contact Us</h1>
+        <section className="w-full max-w-1440 flex min-h-[50vh] flex-col justify-center items-center mx-5 p-2 bg-slate-800 text-cream space-y-10 pink-box-shadow-style">
+            <h1 className='text-cream'>Contact Us</h1> 
             <p className='text-center text-lg md:text-2xl'>
                 Interested in joining our mission or have questions? Contact our president Maximus Powers at 
                 <a href='mailto:maximus@ethicalspectacle.com' className='underline pl-2'>
