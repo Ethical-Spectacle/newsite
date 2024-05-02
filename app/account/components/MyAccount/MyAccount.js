fbf8629e0d7d294f15f8529f7d1657b7f2b5e7ad
Maximus Powers <srewop.xam@gmail.com>
2024-05-01 22:56:12 -0700

    fixed account page, merge messed it up

--- app/account/components/MyAccount/MyAccount.js
+++ app/account/components/MyAccount/MyAccount.js
@@ -3,20 +3,19 @@ import Badges from './Badges';
 import MiniEvents from './MiniEvents';
 import Opportunities from './Opportunities';
 import Certificates from './Certificates';
-import './MyAccount.css';
 
 function MyAccount({ userEmail }) {
   return (
-    <div className="outside-container">
-      <div className='boxed-container flex-col space-y-10 md:space-y-0 md:flex-row md:space-x-28'>
+    <div className="bg-gray-100 min-h-screen">
+      <div className='flex flex-col md:flex-row'>
 
-        <div className="flex-1 space-y-5">
+        <div className="md:flex-1">
           <ProfileInfo userEmail={userEmail} />
           <Certificates userEmail={userEmail} />
           <Badges userEmail={userEmail}/>
         </div>
 
-        <div className="flex-1 space-y-5">
+        <div className="md:flex-1">
           <MiniEvents />
           <Opportunities />
         </div>
