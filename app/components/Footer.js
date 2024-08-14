d0c150acbeb12568d09fd7a9350d34d573779811
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-14 14:42:53 -0400

    Added hover animation to Footer component

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex flex-col w-full">
-      <div className="bg-black grid md:grid-cols-5 p-10 max-md:p-5 gap-5 grid-cols-2 ">
+      <div className="bg-black grid md:grid-cols-4 max-auto px-20 py-14 max-md:p-5 gap-4 grid-cols-2 ">
         <div className="p-5 max-md:col-span-2 max-md:justify-self-center">
           <Image
             src={whitelogo}
@@ -18,62 +18,73 @@ export default function Footer() {
             height={70}
           />
         </div>
-        
+
         <div>
-          <h3 className="text-white text-xl my-2">Legals</h3>
+          <h3 className="text-white text-lg my-2">Legals</h3>
           <Link href="/">
-            <p className="text-sm text-gray-400">Privacy Policy</p>
+            <p className="text-sm text-gray-400 hover:text-white">
+              Privacy Policy
+            </p>
           </Link>
           <Link href="/">
-            <p className="text-sm text-gray-400">Terms & Conditions</p>
+            <p className="text-sm text-gray-400 hover:text-white">
+              Terms & Conditions
+            </p>
           </Link>
         </div>
 
-        <p className="text-sm text-white max-md:col-span-2">
-          maximus@ethicalspectacle.com              
-        </p>
+        <div>
+          <h3 className="text-white text-lg my-2">Contacts</h3>
+          <p className="text-sm text-gray-400 max-md:col-span-2 hover:text-white">
+            maximus@ethicalspectacle.com
+          </p>
+        </div>
 
         <div className="p-5 max-md:col-span-2 max-md:justify-self-center max-md:p-2">
-          <h3 className="text-white text-xl my-2 max-md:hidden">Socials</h3>
           <div className="flex gap-5">
-            <Link href="https://www.linkedin.com/company/ethicalspectacle/about/?viewAsMember=true">
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/company/ethicalspectacle/about/?viewAsMember=true"
+            >
               <Image
                 src={linkedinplain}
                 width={30}
                 height={30}
                 alt="Linkedin Logo"
-                className="text-gray-400"
+                className=""
               ></Image>
             </Link>
-            <Link href="https://huggingface.co/ethical-spectacle">
+            <Link
+              target="_blank"
+              href="https://huggingface.co/ethical-spectacle"
+            >
               <Image
                 src={huggingfacelogo}
                 width={30}
                 height={30}
                 alt="Hugging Face Logo"
-                className="text-gray-400"
               ></Image>
             </Link>
-            <Link href="https://www.meetup.com/ethical-spectacle-research/events/">
+            <Link
+              target="_blank"
+              href="https://www.meetup.com/ethical-spectacle-research/events/"
+            >
               <Image
                 src={meetuplogo}
                 width={30}
                 height={30}
                 alt="Meetup Logo"
-                className="text-gray-400"
               ></Image>
             </Link>
           </div>
         </div>
       </div>
       <div className="block text-center items-center bg-black text-gray-400 py-5">
-          <p className="max-md:text-sm text-gray-400">
-            © 2024 Ethical Spectacle Research.
-          </p>
-          <p className="max-md:text-sm text-gray-400">
-            All rights reserved.
-          </p>
-        </div>
+        <p className="max-md:text-sm text-gray-400">
+          © 2024 Ethical Spectacle Research.
+        </p>
+        <p className="max-md:text-sm text-gray-400">All rights reserved.</p>
+      </div>
     </footer>
   );
 }
